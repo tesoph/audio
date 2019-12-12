97f56b5bfc0c1b16aba193f8861190a885ffef12
Rachel Mc Laughlin <12276029+tesoph@users.noreply.github.com>
2019-12-12 19:02:11 +0000

    Change max number of movers from 75 to 100

--- assets/js/audioVisualizer.js
+++ assets/js/audioVisualizer.js
@@ -21,7 +21,7 @@ let audioVisualizerSketch = function (p) {
         p.initializeVariables();
         //Array of movers initialised with max (100) number of movers
         p.initializeMovers(p.w, p.h);
-        
+
         //Check if the browser supports media stream API and check if given permission
         //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
         let constraints = { audio: true, video: false };
@@ -241,23 +241,24 @@ let audioVisualizerSketch = function (p) {
     //End of Attractor and Mover classes
 
     //Methods belonging to the audio visualizer
+
     /**
- * @function getCanvasSize
- * Gets width and height of the sketch canvas parent container
- * @returns {object}
- * object.x - the width of the container
- * object.y - the height of the container
- */
-p.getCanvasSize= function(){
-    let sketchContainer = document.getElementById('sketch-container');
-    let positionInfo = sketchContainer.getBoundingClientRect();
-    let containerHeight = positionInfo.height;
-    let containerWidth = positionInfo.width;
-    return {
-        x: containerWidth,
-        y: containerHeight,
-    };
-}
+    * @method getCanvasSize
+    * @desc Gets width and height of the sketch canvas parent container
+    * @returns {object}
+    * object.x - the width of the container
+    * object.y - the height of the container
+    */
+    p.getCanvasSize = function () {
+        let sketchContainer = document.getElementById('sketch-container');
+        let positionInfo = sketchContainer.getBoundingClientRect();
+        let containerHeight = positionInfo.height;
+        let containerWidth = positionInfo.width;
+        return {
+            x: containerWidth,
+            y: containerHeight,
+        };
+    }
     /**
     * @method makeShapeMode
     * @param i_ - The index of the mover array
@@ -306,7 +307,7 @@ p.getCanvasSize= function(){
         let weight = [0.3, 0.4, 0.1, 0.1, 0.1];
         //Make new attractor and movers orientated to canvas center
         p.a = new Attractor(p);
-        for (let i = 0; i < 75; i++) {
+        for (let i = 0; i < 100; i++) {
             p.moversLowMid[i] = new Mover(this, getRandomItem(list, weight), p.w / 2 + p.random(-10, 10), p.h / 2, p.myColor);
             p.moversHighMid[i] = new Mover(this, 2, p.w / 2 + p.random(-10, 10), p.h / 2, p.highMidColor);
         }
@@ -442,3 +443,125 @@ p.getCanvasSize= function(){
     };
 };
 //End of audiovisualizer sketch
+
+let settingsMenu = {
+
+    popovers: function () {
+        $(function () {
+            $('[data-toggle="popover"]').popover();
+        });
+        $('.popover-dismiss').popover({
+            trigger: 'focus'
+        });
+    },
+    closeMenu: function () {
+        $("#close-settings").on("click", function () {
+            $("#settings-menu").toggle();
+        });
+    },
+    setTheme: function () {
+        $('input:radio[name ="backgroundColorRadio"]').on('click change', setBackgroundColor);
+
+        function setBackgroundColor() {
+            let bgCol = document.querySelector('input[name="backgroundColorRadio"]:checked').value;
+            if (bgCol === "light") {
+                audioVisualizer.backgroundColor = audioVisualizer.color(255, 255, 255, 5);
+                audioVisualizer.strokeColor = audioVisualizer.color(0, 0, 0);
+            } else if (bgCol === "dark") {
+                audioVisualizer.backgroundColor = audioVisualizer.color(0, 0, 0, 5);
+                audioVisualizer.strokeColor = audioVisualizer.color(255, 255, 255);
+            }
+        }
+    },
+    displayLines: function () {
+        let linesCheckbox = document.getElementById("linesCheckbox");
+
+        linesCheckbox.onchange = function () {
+            displayLines();
+        };
+
+        function displayLines() {
+            if ($('#linesCheckbox').is(":checked")) {
+                audioVisualizer.lines = true;
+            } else {
+                audioVisualizer.lines = false;
+            }
+        }
+    },
+    toggleShapeMode:function(){
+        document.getElementById("shapeCheckbox").onchange = function () {
+            if (this.checked) {
+                audioVisualizer.shapeMode = true;
+            } else {
+                audioVisualizer.shapeMode = false;
+            }
+        };
+    },
+    setSensitivity:function(){
+        let sensitivitySlider = document.getElementById("sensitivity-slider");
+
+        sensitivitySlider.onchange = function () {
+            changeSensitivity();
+        };
+    
+        function changeSensitivity() {
+            audioVisualizer.sensitivity = sensitivitySlider.value;
+    }
+    },
+  setStrokeWeight:function(){
+    let SWslider = document.getElementById("stroke-weight-picker");
+    SWslider.oninput = changeStrokeWeight;
+    function changeStrokeWeight(event) {
+        audioVisualizer.strokeWidth = event.target.value;
+    }
+    
+  },
+setMoverColor:function(){
+    //Color input type polyfill https://bgrins.github.io/spectrum/
+//BUG FIX: defaults to text input for browsers that don't support input type color
+//ERROR [Intervention] Unable to preventDefault inside passive event listener due to target being treated as passive. See <URL>
+$("#lowMidColor").spectrum({
+    move: function (tinycolor) {
+        let c = tinycolor;
+        let alph = c.getAlpha();
+        c.setAlpha(alph);
+        console.log(c.toRgbString());
+        audioVisualizer.myColor = c.toRgbString();
+    },
+    showAlpha: true
+});
+
+$("#highMidColorPicker").spectrum({
+    move: function (tinycolor) {
+        let c = tinycolor;
+        let alph = c.getAlpha();
+        c.setAlpha(alph);
+        console.log(c.toRgbString());
+        audioVisualizer.highMidColor = c.toRgbString();
+    },
+    showAlpha: true
+});
+},
+displayHighMidMovers:function(){
+    document.getElementById("highMidCheckbox").onchange = function () {
+        console.log("checked");
+        if (this.checked == true) {
+            audioVisualizer.displayHighMid = true;
+        } else {
+            audioVisualizer.displayHighMid = false;
+        }
+    };
+},
+setSpeed:function(){
+    let topspeedSlider = document.getElementById("topspeed-slider");
+topspeedSlider.oninput = function () {
+    audioVisualizer.topspeed2 = Math.floor(topspeedSlider.value);
+};
+},
+setNumberOfMovers:function(){
+    let numMovers = document.getElementById("number-of-movers");
+    numMovers.oninput = function () {
+        audioVisualizer.numberOfMovers = Math.floor(numMovers.value);
+    }; 
+},
+};
