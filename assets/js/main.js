fce3f368a867802f600ec33078927b7f1527b4c4
Rachel Mc Laughlin <12276029+tesoph@users.noreply.github.com>
2019-12-27 00:14:22 +0000

    Add CSS and javascript for calculating viewport height

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
 //Audio Visualizer
 
 //The audio visualizer sketch is created in instance mode and attached to the DOM element #sketch-container
@@ -48,6 +54,8 @@ sketchContainer.addEventListener('click', audioVisualizer.play);
 window.addEventListener('resize', resizeCanvas);
 window.addEventListener('orientationchange', resizeCanvas);
 function resizeCanvas() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     let canvasSize = audioVisualizer.getCanvasSize();
     audioVisualizer.resizeCanvas(canvasSize.x, canvasSize.y);
     audioVisualizer.initializeMovers(canvasSize.x, canvasSize.y);
