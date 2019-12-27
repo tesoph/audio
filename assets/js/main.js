254d462666e527929aafb694ff7ac9fd1c9cb97c
Rachel Mc Laughlin <12276029+tesoph@users.noreply.github.com>
2019-12-27 00:29:52 +0000

    Tidy main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
 //Audio Visualizer
 
 //The audio visualizer sketch is created in instance mode and attached to the DOM element #sketch-container
@@ -15,18 +9,6 @@ let myToolbar = new toolbar();
 //Settings Menu
 let mySettingsMenu = new settingsMenu();
 
-/*ettingsMenu.popovers();
-settingsMenu.closeMenu();
-settingsMenu.setTheme();
-settingsMenu.displayLines();
-settingsMenu.toggleShapeMode();
-settingsMenu.setSensitivity();
-settingsMenu.setStrokeWeight();
-settingsMenu.setMoverColor();
-settingsMenu.displayHighMidMovers();
-settingsMenu.setSpeed();
-settingsMenu.setNumberOfMovers();*/
-
 //Events
 
 //On page load the sketch variables are set according to the DOM input values
@@ -35,9 +17,6 @@ window.addEventListener('load', function () {
 });
 
 function setVariables() {
-    //Uncommenting topspeed and sensitivity causes error: p5.Vector.prototype.mult: n is undefined or not a finite numbe
-    // audioVisualizer.sensitivity = $('#sensitivity-slider').val();
-    // audioVisualizer.topspeed2 = $('#topspeed-slider').val();
     audioVisualizer.myColor = $('#lowMidColor').val();
     audioVisualizer.strokeWidth = $("#stroke-weight-picker").val();
     audioVisualizer.highMidColor = $('#highMidColorPicker').val();
@@ -60,6 +39,7 @@ function resizeCanvas() {
     audioVisualizer.resizeCanvas(canvasSize.x, canvasSize.y);
     audioVisualizer.initializeMovers(canvasSize.x, canvasSize.y);
 }
+
 //https://webrtchacks.com/guide-to-safari-webrtc/
 //BUG FIX: for issue on safari when leaving the tab and then returning, the audio stream would be muted and a new stream created which wasn't linked to the FFT object
 //When the window is focused, audioVisualizer.getAudioInput() to create a new audio stream which the sketch then analyzes
@@ -68,3 +48,9 @@ $(window).focus(function (e) {
     console.log("focused");
     audioVisualizer.getAudioInput();
 });
+
+//Bug fix for sketch-container being cut off at the bottom of mobile viewports.
+//Following code for calculating viewport height from: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
